8f1a266681b71d9141f3c6dd7b86d1c245ede8b7
Alessandra <alessm96@gmail.com>
2022-09-05 16:27:53 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let today = new Date();
+let day = today.getDay();
+const daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+
+let hour = today.getHours();
+let minute = today.getMinutes();
+let second = today.getSeconds();
+let ending = (hour >= 12)? " PM ":" AM ";
+  hour = (hour >= 12)? hour - 12: hour;
+
+if (hour === 0 && ending ===' PM ') { 
+    if (minute===0 && second===0){ 
+        hour=12;
+        ending=' Noon';
+    } else { 
+        hour=12;
+        ending=' PM';
+    } 
+} 
+
+if (hour === 0 && ending === ' AM '){ 
+  if (minute === 0 && second === 0){ 
+    hour=12;
+    ending=' Midnight';
+  } else { 
+    hour=12;
+    ending=' AM';
+  } 
+} 
